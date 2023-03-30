b8809877e3c0b7982f4da9480b8fe42af7569b2f
FherEnrique <enquique500@gmail.com>
2023-03-30 14:01:51 -0600

    🤡 Create factories to models

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'SKU' => $this->faker->uuid(), 
+            'name' => $this->faker->name(), 
+            'stock' => $this->faker->numberBetween(1, 20), 
+            'price' => $this->faker->numberBetween(1, 20), 
+            'description' => $this->faker->text(), 
+            'image' => $this->faker->word(),
+        ];
+    }
+}
